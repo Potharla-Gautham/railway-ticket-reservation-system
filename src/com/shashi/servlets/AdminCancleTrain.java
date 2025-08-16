9eedee88cb7979ec97500a35fe5d7d1904d97af2
POTHARLA GAUTHAM <2200033159cser@gmail.com>
2025-08-16 23:06:54 +0530

    Update AdminCancleTrain.java

--- src/com/shashi/servlets/AdminCancleTrain.java
+++ src/com/shashi/servlets/AdminCancleTrain.java
@@ -1,4 +1,4 @@
-package com.shashi.servlets;
+package com.gautham.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,12 +10,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.shashi.beans.TrainException;
-import com.shashi.constant.ResponseCode;
-import com.shashi.constant.UserRole;
-import com.shashi.service.TrainService;
-import com.shashi.service.impl.TrainServiceImpl;
-import com.shashi.utility.TrainUtil;
+import com.gautham.beans.TrainException;
+import com.gautham.constant.ResponseCode;
+import com.gautham.constant.UserRole;
+import com.gautham.service.TrainService;
+import com.gautham.service.impl.TrainServiceImpl;
+import com.gautham.utility.TrainUtil;
 
 @WebServlet("/admincancletrain")
 public class AdminCancleTrain extends HttpServlet {
@@ -57,3 +57,4 @@ public class AdminCancleTrain extends HttpServlet {
 	}
 
 }
+
