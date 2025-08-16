e4f79853fbcb22eec8622e96bc4c6a4aee95aa73
POTHARLA GAUTHAM <2200033159cser@gmail.com>
2025-08-16 23:18:59 +0530

    Update BookTrainFwd.java

--- src/com/shashi/servlets/BookTrainFwd.java
+++ src/com/shashi/servlets/BookTrainFwd.java
@@ -1,4 +1,4 @@
-package com.shashi.servlets;
+package com.gautham.servlets;
 
 import java.io.IOException;
 
@@ -9,8 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.shashi.constant.UserRole;
-import com.shashi.utility.TrainUtil;
+import com.gautham.constant.UserRole;
+import com.gautham.utility.TrainUtil;
 
 @WebServlet("/booktrainfwd")
 public class BookTrainFwd extends HttpServlet {
@@ -29,3 +29,4 @@ public class BookTrainFwd extends HttpServlet {
 	}
 
 }
+
