daa4936781e64f8d0b671cf1940473d281bb9bf0
POTHARLA GAUTHAM <2200033159cser@gmail.com>
2025-08-16 23:33:20 +0530

    Update UserHome.java

--- src/com/shashi/servlets/UserHome.java
+++ src/com/shashi/servlets/UserHome.java
@@ -1,4 +1,4 @@
-package com.shashi.servlets;
+package com.gautham.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,8 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.shashi.constant.UserRole;
-import com.shashi.utility.TrainUtil;
+import com.gautham.constant.UserRole;
+import com.gautham.utility.TrainUtil;
 
 @SuppressWarnings("serial")
 @WebServlet("/userhome")
@@ -33,3 +33,4 @@ public class UserHome extends HttpServlet {
 	}
 
 }
+
