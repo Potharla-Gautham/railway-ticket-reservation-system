9179b45755e3cd6a50d5fb4b91b13bae59155906
POTHARLA GAUTHAM <2200033159cser@gmail.com>
2025-08-16 23:09:57 +0530

    Update AdminSearchTrain.java

--- src/com/shashi/servlets/AdminSearchTrain.java
+++ src/com/shashi/servlets/AdminSearchTrain.java
@@ -1,4 +1,4 @@
-package com.shashi.servlets;
+package com.gautham.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,12 +10,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.shashi.beans.TrainBean;
-import com.shashi.beans.TrainException;
-import com.shashi.constant.UserRole;
-import com.shashi.service.TrainService;
-import com.shashi.service.impl.TrainServiceImpl;
-import com.shashi.utility.TrainUtil;
+import com.gautham.beans.TrainBean;
+import com.gautham.beans.TrainException;
+import com.gautham.constant.UserRole;
+import com.gautham.service.TrainService;
+import com.gautham.service.impl.TrainServiceImpl;
+import com.gautham.utility.TrainUtil;
 
 @SuppressWarnings("serial")
 @WebServlet("/adminsearchtrain")
@@ -60,3 +60,4 @@ public class AdminSearchTrain extends HttpServlet {
 	}
 
 }
+
